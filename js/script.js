d02142f38eaa5971ef6f7a1a132a2a1cbeb57341
Zaid Hassan <zaidhassan251@gmail.com>
2023-08-05 00:31:56 +0530

    Changed Project images, Some monor typography fixes, added scroll navigation

--- js/script.js
+++ js/script.js
@@ -6,3 +6,23 @@ btn.addEventListener('click', () => {
     nav.classList.toggle('flex');
     nav.classList.toggle('hidden');
 })
+
+const body = document.body;
+let lastScroll = 0;
+
+window.addEventListener('scroll', () => {
+    const currentScroll = window.scrollY;
+
+    if (currentScroll <= 0) {
+        body.classList.remove('scroll-up');
+    }
+    if ( currentScroll > lastScroll && !body.classList.contains('scroll-down')) {
+        body.classList.remove('scroll-up');
+        body.classList.add('scroll-down');
+    }
+    if ( currentScroll < lastScroll && body.classList.contains('scroll-down')) {
+        body.classList.remove('scroll-down');
+        body.classList.add('scroll-up');
+    }
+    lastScroll = currentScroll;
+})
